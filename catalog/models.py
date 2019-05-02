10e761148c7ddaf29cb87faa06a6925d6741ec88
Kevin Michael <biobot.mike@gmail.com>
2019-05-02 23:52:48 +0200

    Add user class

--- catalog/models.py
+++ catalog/models.py
@@ -5,5 +5,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(70), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String)
+
+
 engine = create_engine('sqlite:///../motorbike_catalog.db')
 Base.metadata.create_all(engine)
