8b73a4af1d99fb14f6fc6144072add5c4a496fca
Kevin Michael <biobot.mike@gmail.com>
2019-05-03 23:41:50 +0200

    Add columns to motorbike class

--- catalog/models.py
+++ catalog/models.py
@@ -28,7 +28,12 @@ class Motorbike(Base):
     id = Column(Integer, primary_key=True)
     model = Column(String(70), nullable=False)
     engine = Column(String(140), nullable=False)
-    capacity = Column(String(40), nullable=False)
+    displacement = Column(String(7), nullable=False)
+    curb_mass = Column(String(6), nullable=False)
+    fuel_capacity = Column(String(9), nullable=False)
+    max_power = Column(String(9), nullable=False)
+    max_torque = Column(String(10), nullable=False)
+    image = Column(String, nullable=False)
 
 
 engine = create_engine('sqlite:///../motorbike_catalog.db')
