99c7f71ffb1b7003ffa932922f23f60610f97d80
Kevin Michael <biobot.mike@gmail.com>
2019-05-03 23:42:36 +0200

    Add constraint to user picture column

--- catalog/models.py
+++ catalog/models.py
@@ -12,7 +12,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(70), nullable=False)
     email = Column(String(250), nullable=False)
-    picture = Column(String)
+    picture = Column(String, nullable=False)
 
 
 class Manufacturer(Base):
