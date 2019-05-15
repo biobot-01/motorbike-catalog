bd3f043d612ff225676cffd01b93909584002126
Kevin Michael <biobot.mike@gmail.com>
2019-05-15 20:02:36 +0200

    Add create model for new model

--- catalog/app.py
+++ catalog/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from slugify import slugify
 
 from models import Base, Manufacturer, Motorbike
 # Create flask instance
@@ -58,6 +59,38 @@ def model(manufacturer_slug, motorbike_slug):
 def new_model(manufacturer_slug):
     manufacturer = session.query(Manufacturer).filter_by(
         slug=manufacturer_slug).first()
+    if request.method == 'POST':
+        model = request.form['model']
+        year = request.form['year']
+        engine = request.form['engine']
+        displacement = request.form['displacement']
+        max_power = request.form['max_power']
+        max_torque = request.form['max_torque']
+        fuel_capacity = request.form['fuel_capacity']
+        curb_mass = request.form['curb_mass']
+        image = request.form.get('image', '/static/img/default-bike.png')
+        slug = slugify(model + '-' + year)
+        manufacturer_id = manufacturer.id
+        if (model and year and engine and displacement and
+                max_power and max_torque and fuel_capacity and
+                curb_mass and image and slug):
+            model = Motorbike(
+                slug=slug,
+                model=model,
+                year=year,
+                engine=engine,
+                displacement=displacement,
+                curb_mass=curb_mass,
+                fuel_capacity=fuel_capacity,
+                max_power=max_power,
+                max_torque=max_torque,
+                image=image,
+                manufacturer_id=manufacturer_id,
+            )
+            session.add(model)
+            session.commit()
+            return redirect(url_for(
+                'bikes', manufacturer_slug=manufacturer.slug))
     return render_template('new-model.html', manufacturer=manufacturer)
 
 
